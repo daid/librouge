5a741654cffc66a063e7a4203896d080492597e4
Daid <daid303@gmail.com>
2023-12-12 12:37:16 +0100

    .

--- core/include/r/vec.h
+++ core/include/r/vec.h
@@ -2,6 +2,7 @@
 
 #include <glm/vec2.hpp>      // for glm::vec2 and glm::ivec2
 #include <glm/geometric.hpp> // for glm::length
+#include <glm/gtx/hash.hpp>  // for std::hash implementations
 
 namespace r {
     using namespace glm;
--- core/src/line.cpp
+++ core/src/line.cpp
@@ -16,6 +16,7 @@ bool TraceLine::Iterator::operator!=(const Iterator& other) const { return index
 void TraceLine::Iterator::operator++() { index++; }
 ivec2 TraceLine::Iterator::operator*() const {
     auto n = std::max(std::abs(start_point.x - end_point.x), std::abs(start_point.y - end_point.y));
+    if (n == 0) return start_point;
     auto s = vec2{start_point.x + 0.5f, start_point.y + 0.5f};
     auto d = vec2{end_point.x - start_point.x, end_point.y - start_point.y};
     return floor(s + d * float(index) / float(n));
